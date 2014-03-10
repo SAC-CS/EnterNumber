b1cd8553ee59ca76def98a020e5419529c187fcb
diemnguyen3 <diemnguyen_1011@yahoo.com>
2014-03-09 23:52:36 -0700

    Commit

---
+++ src/Enter.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Enter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner input = new Scanner(System.in);
+		int number1;
+		int number2;
+		
+		System.out.println("Enter first integer");
+		number1=input.nextInt();
+
+		System.out.println("Enter second integer");
+		number2=input.nextInt();
+		
+		System.out.printf("Hello, yeu enter",number1,"and",number2);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
